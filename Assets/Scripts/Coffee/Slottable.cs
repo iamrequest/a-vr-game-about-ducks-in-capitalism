3a11fb45e2b205306e63fdb5bf26f0875de53db3
request <iamrequest@protonmail.com>
2020-07-26 00:03:03 -0400

    Slottable objects can now pseudo-parent to slots

--- Assets/Scripts/Coffee/Slottable.cs
+++ Assets/Scripts/Coffee/Slottable.cs
@@ -10,11 +10,17 @@ public class Slottable : MonoBehaviour {
     private Rigidbody rb;
     private Interactable interactable;
     public GameObject slot;
-    public bool isSlotted;
+    public bool isSlotted, followSlotWhenSlotted;
+    private bool isLerping;
     public float lerpDuration = 1f;
     [Tooltip("The delay between a grab, and being able to slot this object again")]
     public float postGrabSnapDelay;
     private float timeSinceLastGrab;
+
+    [Header("Release")]
+    public bool releaseWhenInverted;
+    [Range(0f, 1f)]
+    public float minReleasePercentage;
 
     private void Start() {
         rb = GetComponent<Rigidbody>();
@@ -24,6 +30,33 @@ public class Slottable : MonoBehaviour {
         if (timeSinceLastGrab < 999) {
             timeSinceLastGrab += Time.deltaTime;
         }
+
+        // Follow the slot's transform, if the option's enabled. Only do so if we're slotted, and not currently lerping
+        //  Likely would be better to do this via rb.MovePosition() and MoveRotation(), and in FixedUpdate(), but there's some jankiness by doing it that way
+        //  - rb still has gravity, so this gameobject falls through the floor for some reason
+        //  - rb MovePosition() jitters, since it's still doing collisions
+        //
+        //  Doing it this way is bad because rb calculations are still being made in FixedUpdate(). Good old gamejam jank~
+        if (isSlotted && !isLerping && followSlotWhenSlotted) {
+            transform.position = slot.transform.position;
+            transform.rotation = slot.transform.rotation;
+        }
+    }
+
+    private void FixedUpdate() {
+        if (isSlotted && !isLerping) {
+            // Attempt to release the slot
+            if (releaseWhenInverted) {
+                // Ranges from [-1, 1], where 1 represents fully up
+                float upSimilarity = Vector3.Dot(transform.up, Vector3.up);
+
+                // Next, we'll remap it to [0, 1], where 0 means completely downwards
+                float upPercentage = (upSimilarity * .5f) + 0.5f;
+                if (upPercentage < minReleasePercentage) {
+                    ReleaseFromSlot();
+                }
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -32,6 +65,7 @@ public class Slottable : MonoBehaviour {
         if (timeSinceLastGrab > postGrabSnapDelay && !isSlotted) {
             if(other.gameObject == slot) {
                 isSlotted = true;
+                isLerping = true;
                 rb.isKinematic = true;
 
                 StartCoroutine(DoSlotLerp());
@@ -40,7 +74,7 @@ public class Slottable : MonoBehaviour {
     }
 
     // Hook this up to the interactable OnPickUp() event
-    public void OnGrab() {
+    public void ReleaseFromSlot() {
         // If we're releasing the object from its slot (ie: first grab after being slotted)
         if (isSlotted) {
             timeSinceLastGrab = 0f;
@@ -77,6 +111,7 @@ public class Slottable : MonoBehaviour {
         rb.velocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
 
+        isLerping = false;
         yield break;
     }
 }
