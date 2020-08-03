b3040aa6f0a1c7533fa3d67ad906895348e1b514
request <iamrequest@protonmail.com>
2020-08-03 14:31:23 -0400

    More work on the diner scene, killzones
    - Added killzone listener script. Players can't yeet things across the diner and softlock now
    - Added coffee brewing stuff to the diner scene. Slots are kinda broken right now

---
+++ Assets/Scripts/KillzoneListener.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// On Trigger Enter with a killzone, this gameobject will snap back to its original position/rotation
+public class KillzoneListener : MonoBehaviour {
+    private const string KILLZONE_LAYER = "Killzone";
+    private Rigidbody rb;
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
+
+    private void Start() {
+        originalPosition = transform.position;
+        originalRotation = transform.rotation;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer(KILLZONE_LAYER)) {
+            transform.position = originalPosition;
+            transform.rotation = originalRotation;
+
+            // If this gameobject has a rigidbody at its root, then reset its positional/angular velocity
+            if (rb != null) {
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+        }
+    }
+}
